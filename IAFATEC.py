8550f62a2d01993c9c490c393f36902c0d8943cc
HarleyHell <71992603+HarleyHell@users.noreply.github.com>
2024-06-03 17:12:17 -0300

    Add files via upload

---
+++ IAFATEC.py
@@ -0,0 +1,23 @@
+
+#Carregando o conjunto de dados
+import pandas as pd
+arquivo = pd.read_csv("C:/Users/Nill/Desktop/wine_dataset.csv")
+
+arquivo.head()
+
+arquivo['style'] = arquivo['style'].replace('red', 0)
+arquivo['style'] = arquivo['style'].replace('white', 1)
+
+y = arquivo['style']
+x = arquivo.drop('style', axis = 1)
+
+from sklearn.model_selection import train_test_split 
+x_treino, x_teste, y_teste, y_teste = train_test_split(x, x, test_size = 0.3)
+
+from sklearn.ensemble import ExtraTreesClassifier
+modelo = ExtraTreesClassifier()
+modelo.fit(x_treino, y_treino)
+
+resultado = modelo.score(x_teste, y_teste)
+print("Acurácia:", resultado)
+
